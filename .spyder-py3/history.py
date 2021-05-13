111dd2082ff33348dfb7aaaa1e5f22dad58f39ad
Madie-Bland <madie.bland@gmail.com>
2021-05-13 10:21:39 +1000

    created simple python script equation

---
+++ .spyder-py3/history.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed May 12 09:55:56 2021)---
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+my_string='Legend'
+print(len(my_string))
+print(my_string.swapcase())
+radiance = 1.0
+radiance = max(2.1, 2.0 + min(radiance, 1.1 * radiance - 0.5))
+runfile('C:/Users/madie/.spyder-py3/temp.py', wdir='C:/Users/madie/.spyder-py3')
+runcell(0, 'C:/Users/madie/.spyder-py3/temp.py')
+radiance = 1.0
+radiance = max(2.1, 2.0 + min(radiance, 1.1 * radiance - 0.5))
+radiance = 1.0
+radiance = max(2.1, 2.0 + min(radiance, 1.1 * radiance - 0.5))
+print(radiance)
+import math
+pi()
+import math
+print("pi is", maths.pi)
+print("pi is", math.pi)
+help(math)
+from math import cos, pi
+
+print("pi is", pi)
+print('cos(pi0 is', cos(pi))
+from math import cos, pi
+
+print("pi is", pi)
+print('cos(pi) is', cos(pi))
+import math as m
+print("pi is", m.pi)
+print('cos(pi) is',m.cos(m.pi))
+help(math)
+ldexp(4*)
+ldexp(4*4)
+ldexp(4**4)
+ldexp(4)
+ldexp(x:4)
+print((16*16)/(16*16))
+sqrt(16)
+from math import sqrt
+sqrt(16)
+sqrt(25)
+x = 3, y = 9
+print(y/x)
+x = 3
+y = 9
+print(y/x)
+from math import pow
+x = 25
+pow(x,0.5)
+import math as m
+angle = m.degrees(m.pi / 2)
+print(m.degrees(m.pi / 2))
+import math as m
+angle = m.degrees(m.pi / 2)
+print(m.pi / 2))
+import math as m
+angle = m.degrees(m.pi / 2)
+print(m.pi / 2)
+import math as m
+angle = m.degrees(m.pi / 2)
+print(m.degrees(m.pi / 2))
+import math as m
+angle = m.degrees(m.pi / 2)
+print(angle)
+from math import sin, pi
+print("sin(pi/2) =", sin(pi/2))
---
+++ .spyder-py3/temp.py
@@ -0,0 +1,7 @@
+import math as m
+angle = m.degrees(m.pi / 2)
+print(angle)
+
+from math import sin, pi
+print("sin(pi/2) =", sin(pi/2))
+
